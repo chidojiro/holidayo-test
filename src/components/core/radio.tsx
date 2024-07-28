94491a9833d9c74cae407a03ef4c69f080d0bf87
Nick <chidojiro@gmail.com>
2024-07-28 20:12:18 +0700

    Radio component

---
+++ src/components/core/radio.tsx
@@ -0,0 +1,20 @@
+export type RadioProps = {
+  value?: string;
+  checked?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Radio({ value, checked, onChange }: RadioProps) {
+  return (
+    <label className='rounded-full w-6 h-6 flex items-center justify-center border border-gray-300 p-1 cursor-pointer'>
+      <input
+        type='radio'
+        value={value}
+        checked={checked}
+        onChange={onChange}
+        className='peer w-0 h-0 opacity-0 overflow-hidden'
+      />
+      <div className='w-full h-full rounded-full bg-cyan-400 opacity-0 peer-checked:opacity-100 transition-opacity'></div>
+    </label>
+  );
+}
