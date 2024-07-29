c1b55369e15e3c89847c3e606ff5a11b86504698
Nick <chidojiro@gmail.com>
2024-07-29 12:46:45 +0700

    Improve UX

--- src/components/booking/booking-meal-type-option.tsx
+++ src/components/booking/booking-meal-type-option.tsx
@@ -24,26 +24,28 @@ export default function BookingMealTypeOption({
   newSearchParams.set(MEAL_TYPE_SEARCH_PARAM_KEY, kebabCase(mealType));
 
   return (
-    <Link
-      scroll={false}
-      href={'?' + newSearchParams}
-      className={clsx(
-        'flex gap-5 justify-between items-center w-full rounded-xl p-2 pl-9 pr-7 min-h-20 bg-white text-left border',
-        selected ? 'border-cyan-400' : 'border-gray-300',
-      )}
-    >
-      <div className='flex items-center gap-10'>
-        <div className='w-8 h-8 flex items-center justify-center shrink-0'>
-          <IconMeals24 className='text-cyan-400 scale-150' />
+    <label className='block'>
+      <Link
+        scroll={false}
+        href={'?' + newSearchParams}
+        className={clsx(
+          'flex gap-5 justify-between items-center w-full rounded-xl p-2 pl-9 pr-7 min-h-20 bg-white text-left border',
+          selected ? 'border-cyan-400' : 'border-gray-300',
+        )}
+      >
+        <div className='flex items-center gap-10'>
+          <div className='w-8 h-8 flex items-center justify-center shrink-0'>
+            <IconMeals24 className='text-cyan-400 scale-150' />
+          </div>
+          <div className='max-w-[420px]'>
+            <h4 className='font-semibold text-lg'>{mealType}</h4>
+          </div>
         </div>
-        <div className='max-w-[420px]'>
-          <h4 className='font-semibold text-lg'>{mealType}</h4>
+        <div className='flex items-center gap-7'>
+          <Price price={priceDifference} variant='increase' />
+          <Radio checked={selected} />
         </div>
-      </div>
-      <div className='flex items-center gap-7'>
-        <Price price={priceDifference} variant='increase' />
-        <Radio checked={selected} />
-      </div>
-    </Link>
+      </Link>
+    </label>
   );
 }
--- src/components/booking/booking-room-type-option.tsx
+++ src/components/booking/booking-room-type-option.tsx
@@ -24,26 +24,28 @@ export default function BookingRoomTypeOption({
   newSearchParams.set(ROOM_TYPE_SEARCH_PARAM_KEY, kebabCase(roomType));
 
   return (
-    <Link
-      scroll={false}
-      href={'?' + newSearchParams}
-      className={clsx(
-        'flex gap-5 justify-between items-center w-full rounded-xl p-2 pl-9 pr-7 min-h-20 bg-white text-left border',
-        selected ? 'border-cyan-400' : 'border-gray-300',
-      )}
-    >
-      <div className='flex items-center gap-10'>
-        <div className='w-8 h-8 flex items-center justify-center shrink-0'>
-          <IconStay className='text-cyan-400 scale-150' />
+    <label className='block'>
+      <Link
+        scroll={false}
+        href={'?' + newSearchParams}
+        className={clsx(
+          'flex gap-5 justify-between items-center w-full rounded-xl p-2 pl-9 pr-7 min-h-20 bg-white text-left border',
+          selected ? 'border-cyan-400' : 'border-gray-300',
+        )}
+      >
+        <div className='flex items-center gap-10'>
+          <div className='w-8 h-8 flex items-center justify-center shrink-0'>
+            <IconStay className='text-cyan-400 scale-150' />
+          </div>
+          <div className='max-w-[420px]'>
+            <h4 className='font-semibold text-lg line-clamp-2'>{roomType}</h4>
+          </div>
         </div>
-        <div className='max-w-[420px]'>
-          <h4 className='font-semibold text-lg line-clamp-2'>{roomType}</h4>
+        <div className='flex items-center gap-7'>
+          <Price price={priceDifference} variant='increase' />
+          <Radio checked={selected} />
         </div>
-      </div>
-      <div className='flex items-center gap-7'>
-        <Price price={priceDifference} variant='increase' />
-        <Radio checked={selected} />
-      </div>
-    </Link>
+      </Link>
+    </label>
   );
 }
